e66226c9bd9a3cd15968ca75cee4262ccebe71ab
Joseph Lin <54335291+joslinmicrosoft@users.noreply.github.com>
2021-03-10 17:08:01 -0800

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Look at this really cool spinny thing we have here. 3/01/2021 15:15
+          Look at this really cool spinny thing we have here. 3/10/2021 15:07
         </p>
         <a
           className="App-link"
